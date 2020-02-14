cf4d4dbde9ba7e1b04fd2ab9cfa747746b075714
Danasawin Saichomphu <danasawin.saichomphu@fdmgroup.com>
2020-02-14 02:15:28 +0000

    First commit

---
+++ src/main/java/com/fdmgroup/FairBnBwebsite/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.fdmgroup.FairBnBwebsite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	//Landing Page
+			@GetMapping("/")
+			public String home(Model model){
+				return "home";
+			}
+
+}
